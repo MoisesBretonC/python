a570434371db0b090b5b69db43ecc7cffc3192cc
MoisesBretonC <moisesbreton11@gmail.com>
2025-09-19 18:40:12 -0600

    segundo commit

--- Dia4/Juego.py
+++ Dia4/Juego.py
@@ -1,7 +1,9 @@
 from random import *
 
-
+#aqui estamos haciendo la variable de intentos que es igual a 0
+#manejar la variable como 0 en este porque solo se almacena un valor numerico
 intentos = 0
+estimado = 0
 nombre = input('Ingrese su nombre: ')
 #aqui estamos haciendo que se escoja un numero de entre 1 y 100 y se almacene en la variable numero_aleatorio
 numero_aleatorio = randint(1,100)
@@ -9,14 +11,18 @@ print(f"Hola {nombre} he pensando en un numero entre 1 y 100, tienes 8 intentos 
 
 #aqui estamos diciendo que mientras intentos sea menor que 8 todavia tenemos vidas
 while intentos < 8:
-    estimado = int(input("cual es el numero?: "))
+    estimado = int(input("Cual es el numero?: "))
     #aqui vamos añadiendo un intento mas, osea vamos perdiendo una vida
     intentos += 1
-    if estimado < numero_aleatorio:
+    #aqui esta haciendo que el if haga que nomas tenga numeros del 1 al 100
+    if estimado not in range(1,101):
+        print("El numero que ingresaste no se puede usar ")
+    elif estimado < numero_aleatorio:
         print("mi numero es mas alto")
-    if estimado > numero_aleatorio:
+    elif estimado > numero_aleatorio:
         print("mi numero es menor")
-    if estimado == numero_aleatorio:
+    #aqui  iba esto if estimado == numero_aleatorio: pero lo vamos a sustituir por un else
+    else:
         print(f"felicidades {nombre} has encontrado el numero {numero_aleatorio} en tu intento {intentos}")
         break
 
